15e4db1f81c01dfc2b3a77b57e98c0364ab00083
Tim Ashton <tim.ashton.la@gmail.com>
2026-04-16 19:25:41 -0700

    Simplify repos

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# Welcome to your CDK TypeScript project
+# Occupy
 
-This is a blank project for CDK development with TypeScript.
+This is the infrastructure for Occupy.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+# v0 UI
+
+![v0 UI WireFrame](./assets/occupy-ui-wireframe.png)
+
+# v0 Architecture
+
+![v0 Architecture](./assets/v0-architecture.png)
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `npx cdk deploy` deploy this stack to your default AWS account/region
+- `npx cdk diff` compare deployed stack with current state
+- `npx cdk synth` emits the synthesized CloudFormation template
--- lib/occupy-cdk-stack.ts
+++ lib/occupy-cdk-stack.ts
@@ -1,16 +1,50 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { OpenApiGatewayToLambda } from '@aws-solutions-constructs/aws-openapigateway-lambda';
+import { LambdaToDynamoDB } from '@aws-solutions-constructs/aws-lambda-dynamodb';
+import { Asset } from 'aws-cdk-lib/aws-s3-assets';
+import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 
 export class OccupyCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const tableProps = {
+            partitionKey: {
+                name: 'Id',
+                type: ddb.AttributeType.STRING,
+            },
+        };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OccupyCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const recordApparatus = new LambdaToDynamoDB(this, 'Games', {
+            lambdaFunctionProps: {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                handler: 'index.handler',
+                code: lambda.Code.fromAsset(`lambda/game`),
+            },
+            dynamoTableProps: tableProps,
+        });
+
+        const newApi = new OpenApiGatewayToLambda(
+            this,
+            'OpenApiGatewayToLambda',
+            {
+                apiDefinitionAsset: new Asset(this, 'ApiDefinitionAsset', {
+                    path: path.join(`api`, 'occupy.yml'),
+                }),
+                apiIntegrations: [
+                    {
+                        id: 'GameHandler',
+                        existingLambdaObj: recordApparatus.lambdaFunction,
+                    },
+                ],
+            },
+        );
+
+        new cdk.CfnOutput(this, 'GameUrl', {
+            value: newApi.apiGateway.url + 'order',
+        });
+    }
 }
